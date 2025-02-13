0351672b509d3db2074474641bf23b2a1a5894f1
Leonardo Bishop <me@leonardobishop.com>
2025-02-13 00:35:29 +0000

    Change screenlocker and update screenshot program

--- bin/exit-menu-sway.sh
+++ bin/exit-menu-sway.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-entries="  Lock\n󰹆  Suspend\n󰹆  Suspend-then-Hibernate\n󰒲  Hibernate\n󰍃  Logout\n󰜉  Reboot\n󰐥  Shutdown"
+#entries="  Lock\n󰹆  Suspend\n󰹆  Suspend-then-Hibernate\n󰒲  Hibernate\n󰍃  Logout\n󰜉  Reboot\n󰐥  Shutdown"
+entries="  Lock\n󰹆  Suspend\n󰒲  Hibernate\n󰍃  Logout\n󰜉  Reboot\n󰐥  Shutdown"
 
 selected=$(echo -e $entries|wofi --width 100 --height 212 --dmenu --cache-file /dev/null --define content_halign=center --define prompt="Exit menu" | awk '{print tolower($2)}')
 
 case $selected in
   lock)
-    swaylock;;
+    hyprlock;;
   logout)
     hyprctl dispatch exit;;
-  suspend-then-hibernate)
-    exec systemctl suspend-then-hibernate;;
   suspend)
-    exec systemctl suspend;;
+    exec systemctl suspend-then-hibernate;;
   hibernate)
     exec systemctl hibernate;;
   reboot)
