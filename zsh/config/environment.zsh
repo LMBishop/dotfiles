f35f07a9a742a9cc80f61495fa01a8d86bba2f56
Leonardo Bishop <me@leonardobishop.com>
2025-02-13 00:36:12 +0000

    Add gems to path

--- zsh/config/environment.zsh
+++ zsh/config/environment.zsh
@@ -6,10 +6,13 @@ ZSH_CONFIG=$HOME/.config/zsh
 export ZSH="$HOME/.oh-my-zsh"
 
 # User /bin
-export PATH="$HOME/bin:$HOME/bin/compiled:$PATH:/home/leonardo/.local/bin"
+export PATH="$HOME/bin:$HOME/bin/compiled:$PATH:/home/leonardo/.local/bin:/home/leonardo/.local/share/gem/ruby/3.3.0/bin"
 
 # Exa colours
 export EXA_COLORS="uu=37:un=37:gu=37:gn=37:da=37"
 
 # Firefox wayland
 export MOZ_ENABLE_WAYLAND=1
+
+# bundle
+export GEM_HOME=$HOME/.gem
