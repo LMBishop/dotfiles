ca6faa5976dd35706fbc1a17e52a5ae71c24d363
Leonardo Bishop <me@leonardobishop.net>
2026-08-27 12:55:37 +0100

    Update zsh

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -7,12 +7,12 @@ alias vimdiff="nvim -d"
 alias python="python3"
 
 # Ls to exa
-alias ls="exa --icons"                                                         
-alias ll="exa -lbhF --icons --git"                                             
-alias llm="exa -lbhd --icons --git --sort=modified"                            
-alias la="exa -lbahF --icons --git"                                           
-alias lA="exa -lbhHigUmuSa --icons --time-style=long-iso --git --color-scale" 
-alias lx="exa -lbhHigUmuSa@ --icons --time-style=long-iso --git --color-scale"
+alias ls="exa --icons always"                                                         
+alias ll="exa -lbhF --icons always --git"                                             
+alias llm="exa -lbhd --icons always --git --sort=modified"                            
+alias la="exa -lbahF --icons always --git"                                           
+alias lA="exa -lbhHigUmuSa --icons always --time-style=long-iso --git --color-scale" 
+alias lx="exa -lbhHigUmuSa@ --icons always --time-style=long-iso --git --color-scale"
 alias lS="exa -1"                                                             
 alias lt="exa --tree --level=2"                                        
 
